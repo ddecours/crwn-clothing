06518515dbb4b860c7abdec8a5a0bcc50eedc6db
ddecours <doug_decoursin@hotmail.com>
2021-01-13 07:49:23 -0600

    updating our application with reselect, replaced cart selectors with memoized selectors

---
+++ play.js
@@ -0,0 +1,22 @@
+function addTo80(n) {
+    console.log('long time...')
+    return n + 80
+}
+
+let cache = {}
+
+function memoizeAddTo80(n) {
+    if (n in cache) {
+        return cache[n]
+    } else {
+        console.log('long time...')
+        cache[n] = n + 80
+        return cache[n]
+    }
+
+}
+
+console.log('1', memoizeAddTo80(5))
+console.log('2', memoizeAddTo80(6))
+console.log('1', memoizeAddTo80(5))
+console.log('2', memoizeAddTo80(6))
